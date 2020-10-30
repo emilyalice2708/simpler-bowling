a1131e8abe37c2be6d391c1deca067a16c54ca9e
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-30 22:53:15 +0000

    Bowling can successfully score spare bonuses before final frame

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -2,14 +2,25 @@ class Bowling
 
   def initialize
     @score = 0
+    @rolls = []
   end
 
   def bowl(pins)
-    @score += pins
+    @rolls << pins
   end
 
   def score
+    roll_number = 0
+    rolls_length = @rolls.size 
+    while roll_number < rolls_length do
+      if @rolls[roll_number] + @rolls[roll_number] == 10
+        @score += 10 + @rolls[roll_number + 2]
+      else
+        @score += @rolls[roll_number] + @rolls[roll_number + 1]
+      end
+      roll_number += 2
+    end
     @score
   end
-  
+
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -18,7 +18,14 @@ describe Bowling do
   end
 
   it 'can score a game of all 2s' do
-    20.times {game.bowl(2)}
+    20.times { game.bowl(2) }
     expect(game.score).to eq 40
+  end
+
+  it 'can score a spare, followed by a 2' do
+    2.times { game.bowl(5) }
+    game.bowl(2)
+    game.bowl(0)
+    expect(game.score).to eq 14
   end
 end
